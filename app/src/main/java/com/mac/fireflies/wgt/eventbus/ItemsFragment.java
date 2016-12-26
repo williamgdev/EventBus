5e6bfdb3d1a042847f3a2ee1a2eb6d980037ee6f
William GT <william.devl@gmail.com>
2016-12-26 04:00:12 -0500

    Add EventBus Library

--- app/src/main/java/com/mac/fireflies/wgt/eventbus/ItemsFragment.java
+++ app/src/main/java/com/mac/fireflies/wgt/eventbus/ItemsFragment.java
@@ -2,10 +2,14 @@ package com.mac.fireflies.wgt.eventbus;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.greenrobot.eventbus.EventBus;
 
 
 /**
@@ -13,6 +17,8 @@ import android.view.ViewGroup;
  */
 public class ItemsFragment extends Fragment {
 
+
+    private Button button;
 
     public ItemsFragment() {
         // Required empty public constructor
@@ -26,4 +32,15 @@ public class ItemsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_items, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = (Button) view.findViewById(R.id.item_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new StringEvent("Hi from the items fragment"));
+            }
+        });
+    }
 }
